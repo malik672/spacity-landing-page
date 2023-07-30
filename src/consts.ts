9d9cb170123ff9053062a9d2a0159a4a24dd0339
malik672 <aremumalik01@gmail.com>
2023-07-30 21:02:42 +0100

    omo

---
+++ src/consts.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'My personal website.';
+export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const USER_NAME = 'IDAN';
+export const RATINGS = [3, 4,4,4,4,4,4,4,4,4,4,4,4,4,4, 5, 2, 4,2,4,5,2,2,2,5]; 
